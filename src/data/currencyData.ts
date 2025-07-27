20593f13000f64724066c892378a6a9b979d8cdc
PRANAY130 <pranayde201@gmail.com>
2025-07-27 08:26:36 +0000

    Automated update of exchange rates

--- src/data/currencyData.ts
+++ src/data/currencyData.ts
@@ -1,4 +1,4 @@
-// Generated on 2025-07-27 02:01:21 UTC
+// Generated on 2025-07-27 08:26:15 UTC
 // This file contains exchange rates for various currencies relative to INR, along with their corresponding countries.
 
 export interface CurrencyData {
@@ -181,4 +181,4 @@ export const exchangeRates: CurrencyData[] = [
   { code: "ZWL", country: "Zimbabwe", rate: 3.2200 },
 ];
 
-export const lastUpdated = "2025-07-27 02:01:21 UTC";
+export const lastUpdated = "2025-07-27 08:26:15 UTC";
